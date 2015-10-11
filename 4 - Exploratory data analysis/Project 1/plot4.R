3696466a2484d2e0f148b133e812a4ebd88a8431
horreya <horreya@users.noreply.github.com>
2015-10-11 09:45:00 +0100

    fixed legend

--- 4 - Exploratory data analysis/Project 1/plot4.R
+++ 4 - Exploratory data analysis/Project 1/plot4.R
@@ -18,14 +18,14 @@ png(file="plot4.png",width=480,height=480,units="px")
 par(mfrow=c(2,2))
 
 #First chart - Global active power
-with(feb2007,plot(DateTime,Global_active_power,type="l",ylab="Global Active Power(kilowatts)",xlab=""))
+with(feb2007,plot(DateTime,Global_active_power,type="l",ylab="Global Active Power",xlab=""))
 #Second chart - Voltage
 with(feb2007,plot(DateTime,Voltage,type="l",ylab="Voltage",xlab="datetime"))
 #Third chart - Energy sub metering
 with(feb2007,plot(DateTime,Sub_metering_1,type="l",ylab="Energy sub metering",xlab=""))
 with(feb2007,lines(DateTime,Sub_metering_2,type="l",col="red"))
 with(feb2007,lines(DateTime,Sub_metering_3,type="l",col="blue"))
-legend("topright",border="transparent",lwd=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+legend("topright",bty="n",lwd=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 #Fourth chart - Global reactive power
 with(feb2007,plot(DateTime,Global_reactive_power,type="l",xlab="datetime"))
 
